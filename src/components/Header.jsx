cffa2ba09c75ba4b7a8a6423030db829276dce94
Manel <manellouti@live.fr>
2025-01-25 18:55:54 +0100

    Add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../components/styles/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Bienvenue sur mon site</h1>
+      <p>Développeur Web | Portfolio</p>
+    </header>
+  );
+};
+
+export default Header;
