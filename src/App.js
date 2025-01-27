3da668c0dfb92af5f3068055dbc9cfa2d6ef8ab5
Manel <manellouti@live.fr>
2025-01-27 13:45:36 +0100

    debug and hide navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
 import React from "react";
 import Header from './components/Header';
+import Navbar from './components/Navbar';
+import './App.css';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+    {/* <Navbar />*/}
     </div>
   );
 }
