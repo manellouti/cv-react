ecfb3889906837bb333350d6fbab7f2c995f6000
Manel <manellouti@live.fr>
2025-01-28 18:56:31 +0100

    modif title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,8 @@ const Home = () => {
   return (
     <div className="home">
       <section className="intro" style={{ backgroundImage: `url(${heroBg})` }}>
-      <h3>Développeur web full stack</h3>
+      <h1>Bonjour, je suis John Doe</h1>
+      <h2>Développeur web full stack</h2>
         <button>En savoir plus</button>
       </section>
     </div>
