ebc93d936536dd93dfb447ce1bd0ee8d618790c8
Manel <manellouti@live.fr>
2025-02-04 21:51:43 +0100

    debug css

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,7 +10,6 @@ const Contact = () => {
         <h1>ME CONTACTER</h1>
         <p>Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact.</p>
         <hr className="blue-line" />
-
         <div className="contact-content">
           {/* Formulaire */}
           <div className="contact-form-section">
